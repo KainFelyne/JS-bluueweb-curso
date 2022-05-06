e7c92433a16d2de11afdbd74d84e0bc64ff217c0
KainFelyne <rainingblues13@gmail.com>
2022-05-06 09:22:25 -0500

    agregado-numero-de-vidas

--- juego-adivina-numero/script.js
+++ juego-adivina-numero/script.js
@@ -1,18 +1,20 @@
 let numeroMaquina = Math.floor(Math.random() * (10 - 1)) + 1;
 
-console.log(numeroMaquina)
+//console.log(numeroMaquina)
 
 let vidas = 3
 
 let numeroUsuario = parseInt(prompt('Adivina el número del 1 al 10: '))
 
 while(numeroMaquina !== numeroUsuario && vidas > 1){
+    
 
     let mensaje = (numeroMaquina > numeroUsuario) ? 'El número es mayor' : 'El número es menor'
     console.log(mensaje)
 
-    console.log("Intentalo otra vez")
-
+    console.log("Intentalo otra vez. Te quedan " + (vidas - 1) + " vidas")
+    
+    
     numeroUsuario = parseInt(prompt('Adivina el número del 1 al 10: '))
 
     vidas --
